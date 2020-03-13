ccb2f4a7806f1218bcafce1f5c738675deac99eb
Evan Elias Young <evanyoung762@gmail.com>
2020-03-13 02:58:59 -0500

    :ok_hand: Google Stylee

--- ranker.py
+++ ranker.py
@@ -5,7 +5,7 @@ Date     : 2020-03-13
 Revision : 2020-03-13
 """
 
-from typing import List, Dict, Tuple
+from typing import List
 
 
 def get_total_rounds(num_options: int) -> int:
@@ -20,22 +20,24 @@ def get_total_rounds(num_options: int) -> int:
     return sum(range(1, num_options))
 
 
-def get_better_option(e: str, f: str, i: int, j: int) -> int:
+def get_better_option(opt1: str, opt2: str, ind1: int, ind2: int) -> int:
     """Determines the better option, given two.
 
     Arguments:
-        e {str} -- The first option.
-        f {str} -- The second option.
-        i {int} -- The first option's index.
-        j {int} -- The second option's index.
+        opt1 {str} -- The first option.
+        opt2  {str} -- The second option.
+        ind1 {int} -- The first option's index.
+        ind2 {int} -- The second option's index.
 
     Returns:
         int -- The index of the bettwe option.
     """
-    print(f'{i + 1:2} -- {e}')
-    print(f'{j + 1:2} -- {f}')
-    choice: int = int(input(f'Which is better {i + 1} or {j + 1}:\n')) - 1
+    print(f'{ind1 + 1:2} -- {opt1}')
+    print(f'{ind2 + 1:2} -- {opt2}')
+    choice: int = int(
+        input(f'Which is better {ind1 + 1} or {ind2 + 1}:\n')) - 1
     return choice
+
 
 def rank_main(options: List[str]) -> List[int]:
     """Ranks the initial collection of options.
@@ -49,13 +51,14 @@ def rank_main(options: List[str]) -> List[int]:
     # The points associated with each option.
     scores: List[int] = [0] * len(options)
 
-    for i, e in enumerate(options):
-        for j, f in enumerate(options):
+    for i, opt1 in enumerate(options):
+        for j, opt2 in enumerate(options):
             # If the options are not the same and haven't already been compared.
             if i != j and j > i:
-                choice: int = get_better_option(e, f, i, j)
+                choice: int = get_better_option(opt1, opt2, i, j)
                 scores[choice] += 1
     return scores
+
 
 def rank_ties(options: List[str], scores: List[int]) -> List[int]:
     """Ranks the ties within the options.
@@ -69,13 +72,15 @@ def rank_ties(options: List[str], scores: List[int]) -> List[int]:
     """
     # While there are duplicate scores in the list.
     while list(set(scores)) != sorted(scores):
-        for i, e in enumerate(scores):
-            for j, f in enumerate(scores):
+        for i, score1 in enumerate(scores):
+            for j, score2 in enumerate(scores):
                 # If the options are not the same and have the same score.
-                if i != j and e == f:
-                    choice: int = get_better_option(options[i], options[j], i, j)
+                if i != j and score1 == score2:
+                    choice: int = get_better_option(options[i], options[j], i,
+                                                    j)
                     scores[choice] += 1
     return scores
+
 
 def sort_scores(options: List[str], scores: List[int]) -> List[int]:
     """Sorts the options into the final comparison.
@@ -93,10 +98,10 @@ def sort_scores(options: List[str], scores: List[int]) -> List[int]:
     sort = reversed([e for _, e in sorted(zipped_pairs)])
     return sort
 
-if __name__ == "__main__":
-    print('Hello, Console!')
-    print()
 
+def main() -> None:
+    """The main of the program.
+    """
     # The options to rank.
     user_options: List[str] = []
     while True:
@@ -114,8 +119,17 @@ if __name__ == "__main__":
 
     # Sort the zip.
     sort = sort_scores(user_options, user_scores)
-    NUM_LEN: int = len(str(len(user_scores)))
-    SCORE_LEN: int = max(len(str(e)) for e in user_scores)
+    num_len: int = len(str(len(user_scores)))
+    score_len: int = max(len(str(e)) for e in user_scores)
 
-    for i, e in enumerate(sort):
-        print(f'{i + 1:{NUM_LEN}} -- {user_scores[user_options.index(e)]:{SCORE_LEN}} -- {e}')
+    for i, opt in enumerate(sort):
+        print(
+            f'{i + 1:{num_len}} -- {user_scores[user_options.index(opt)]:{score_len}} -- {opt}'
+        )
+
+
+if __name__ == "__main__":
+    print('Hello, Console!')
+    print()
+
+    main()
